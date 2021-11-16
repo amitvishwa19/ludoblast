6f65b012da273fed500e847fffd4518280e8d5f4
jaysvishwagit <jaysvishwa@gmail.com>
2021-11-16 15:33:57 +0530

    first commit

---
+++ Assets/Ludo Masters/Scripts/GameUIController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    public static GameUIController instance;
+
+    public Sprite[] background;
+    public Image backgroundHolder;
+
+
+
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+}
